4365e57c0ed47d68b96764f16bfe21b08f740708
Ana-Popilian <ana.popilian@icloud.com>
2020-03-17 16:40:25 +0100

    Add setup for programmatic approach (deleted Main.storyboard and SceneDelegate) and references  from Info.plist

---
+++ FoodRecipes/FoodRecipes/RecipesListView.swift
@@ -0,0 +1,11 @@
+//
+//  RecipesListView.swift
+//  FoodRecipes
+//
+//  Created by Ana on 17/03/2020.
+//  Copyright © 2020 Ana Popilian. All rights reserved.
+//
+
+import UIKit
+
+
--- FoodRecipes/FoodRecipes/RecipesListViewController.swift
+++ FoodRecipes/FoodRecipes/RecipesListViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecipesListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    view.backgroundColor = .yellow
   }
 
 
